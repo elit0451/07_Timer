4d34228fe605006f896d4f7643d52a953931dec1
Elitsa Marinovska <Elitsa Marinovska@DESKTOP-03FMA85>
2016-09-20 14:47:57 +0200

    Add project files.

---
+++ 07_Timer/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _07_Timer
+{
+    internal class Timer
+    {
+        private int seconds = 0;
+        private int minutes = 0;
+        private int hours = 0;
+        public int Seconds
+        {
+            get { return seconds; }
+             set { seconds = value; }
+        }
+        public override string ToString()
+        {
+            minutes = seconds / 60;
+            seconds = seconds - (minutes * 60);
+            hours = minutes / 60;
+            minutes = minutes - (hours * 60);
+            string result = string.Format("{0:00}:{1:00}:{2:00}", hours, minutes, seconds);
+            return result;
+        }
+
+        internal string Padded(int v)
+        {
+            
+            string res = string.Format("{0:00}",v);
+            return res;
+        }
+    }
+}
